70ecaf513afc4dce80461efa7ae7909da648ccec
jenniferasmar <60263256+jenniferasmar@users.noreply.github.com>
2021-03-29 20:09:35 -0400

    commented out ArithmeticOperationsTest.test

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -1,31 +1,31 @@
-package soen.tutorial;
+// package soen.tutorial;
 
-import org.junit.Test;
+// import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertEquals;
 
-public class ArithmeticOperationsTest {
+// public class ArithmeticOperationsTest {
 
 
-	@Test
-	public void testAdd()
-	{
+// 	@Test
+// 	public void testAdd()
+// 	{
 
-		ArithmeticOperations operations = new ArithmeticOperations();
-		Integer actual = operations.addOrSub(2, 6);
-		Integer expected = 8;
-		assertEquals(expected, actual);
+// 		ArithmeticOperations operations = new ArithmeticOperations();
+// 		Integer actual = operations.addOrSub(2, 6);
+// 		Integer expected = 8;
+// 		assertEquals(expected, actual);
 
-	}
-    @Test
-    public void testSub()
-    {
+// 	}
+//     @Test
+//     public void testSub()
+//     {
 
-        ArithmeticOperations operations = new ArithmeticOperations();
-        Integer actual = operations.addOrSub(6, 2);
-        Integer expected = 4;
-        assertEquals(expected, actual);
+//         ArithmeticOperations operations = new ArithmeticOperations();
+//         Integer actual = operations.addOrSub(6, 2);
+//         Integer expected = 4;
+//         assertEquals(expected, actual);
 
-    }
+//     }
 
-}
+// }
